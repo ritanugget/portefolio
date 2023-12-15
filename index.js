c1a25e76a39b49b0a1722b5074e9fed2b2de012f
ritanugget <114027644+ritanugget@users.noreply.github.com>
2023-12-15 18:02:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//accordion menu//
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+
+
+//slideshow imgs//
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);                            // change image seconds //
+}
